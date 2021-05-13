a4b867a7babd7cd7af9367c87a8abbc11a3cfa0a
Xuming <voicevon@gmail.com>
2021-05-13 18:22:16 +0800

    rename

---
+++ vision/robot_eye.py
@@ -0,0 +1,26 @@
+
+from math import sin, cos
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+
+
+class MonoEye():
+    '''
+        take picture from Pi camera, 
+        Find aruco corners
+        do perspective transforming.
+    '''
+
+    def __init__(self):
+        self.__camera = PiCamera()
+
+    def take_picture(self):
+        rawCapture = PiRGBArray(self.__camera)
+        # grab an image from the camera
+        self.__camera.capture(rawCapture, format="bgr")
+        image = rawCapture.array
+        return image
+
+class SteroEye():
+    pass
