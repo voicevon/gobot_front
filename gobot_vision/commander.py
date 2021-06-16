e1bf7e6620b5d7e32dc638f88f86849d6bab3bef
xuming <voicevon@gmail.com>
2021-06-16 07:36:41 +0100

    fixed small bugs

--- gobot_vision/commander.py
+++ gobot_vision/commander.py
@@ -1,12 +1,15 @@
+import cv2
 from vision.grid_finder import GridFinder
 from gobot_vision.commander_vision import config_2_aruco_marks
-
+import sys
+sys.path.append('/home/pi/pylib')
+from mqtt_helper import g_mqtt
 
 class Commander(GridFinder):
     def __init__(self):
         # self.__mark_ids = [125,126]  # [top, bottom]
-        #self.__CELLS = 5
-        #self.__LOWEST_SCALE = 0.7
+        self.__CELLS = 5
+        self.__LOWEST_SCALE = 0.7
         GridFinder.__init__(self, config_2_aruco_marks)
 
     def get_command_from_image(self, image):
@@ -14,28 +17,37 @@ class Commander(GridFinder):
         return -1, if found nothing
         return 1..5 if found avaliable cell in black.
         '''
-        centers = self.find_corners(image)
-        if len(centers)==2:
-            
-            print('centers',centers)
-            top_x, top_y = centers[0]
-            bottom_x, bottom_y = centers[1]
-            delta_x = (top_x - bottom_x) / 6
-            delta_y = (top_y - bottom_y) / 6
+        corners = self.find_corners(image)
+        #print ('len(centers) =', len(centers))
+        #print(centers)
+        if len(corners) == 4: # Sucessful detection will return 4 corners
+            print('corners',corners)
 
-            if False:
+            x1, y1 = corners[0]
+            x2, y2 = corners[1]
+            x3, y3 = corners[2]
+            x4, y4 = corners[3]
+            cx1 = int((x1 + x2) / 2)
+            cy1 = int((y1 + y2) / 2)
+            cx2 = int((x3 + x4) / 2)
+            cy2 = int((y3 + y4) / 2)
+            
+            delta_x = (cx2 - cx1) / 5 
+            delta_y = (cy2 - cy1) / 5
+
+            if True:
                 # publish a debug image
                 color_red = (0,0,255)
                 pen_thickness = 3
-                cv2.line(image,(top_x,top_y),(bottom_x,bottom_y),color_red, pen_thickness)
+                cv2.line(image,(cx1, cy1),(cx2, cy2),color_red, pen_thickness)
                 g_mqtt.publish_cv_image('gobot/test/image',image)
 
             # Get average color
             sum_color = 0
-            for index in range(1, self.__CELLS + 1,1):
+            for index in range(0, self.__CELLS + 1,1):
                 # get x,y position of indicator
-                x = int(index * delta_x + bottom_x) 
-                y = int(index * delta_y + bottom_y) 
+                x = int(index * delta_x + cx1 + delta_x / 2)
+                y = int(index * delta_y + cy1 + delta_y / 2) 
                 [b,g,r] = image[y, x]
                 sum_color += b
                 sum_color += g
@@ -47,8 +59,8 @@ class Commander(GridFinder):
             
             # Get which cell is black
             for index in range(1, self.__CELLS + 1, 1):
-                x = int(index * delta_x + bottom_x)
-                y = int(index * delta_y + bottom_y)
+                x = int(index * delta_x + cx1 + delta_x / 2) 
+                y = int(index * delta_y + cy1 + delta_y / 2)
                 [b,g,r] = image[y,x]
                 cell_color = b + g + r
                 if cell_color < avg_color * self.__LOWEST_SCALE:
