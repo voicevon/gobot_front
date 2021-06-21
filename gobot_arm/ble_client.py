3a397c752b20a87bc64dbcc4dd323ce8fb901a59
xuming <voicevon@gmail.com>
2021-06-21 04:07:51 +0100

    minus

--- gobot_arm/ble_client.py
+++ gobot_arm/ble_client.py
@@ -1,31 +1,59 @@
 from bluepy import btle
-from bluepy import Scanner
+from bluepy.btle import Scanner
+from bluepy.btle import Peripheral, DefaultDelegate
+
+class MyDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleNotification(self, cHandle, data):
+        print('Notification is invoked')
 
 class ble():
     def __init__(self):
         self.__server_mac = 'b4:e6:2d:b2:f8:8f'
+        #:w
+        #self.__n
 
     def scan(self):
-        scanner = bluepy.Scanner()
+        scanner = Scanner()
         devices = scanner.scan(timeout = 3)
-        print('Found %d devices in %d seconds' % (len(devices), timeout))
+        print('Scan result:  Found %d devices in %d seconds------------' % (len(devices), 3))
         for dev in devices:
             print('Name', dev.getValueText(9))
             print('Address:', dev.addr)
 
-    def connect_to_server(self):
-        pass
-
     def list_services_on_server(self):
-        dev = btle.Peripheral(self.__server_mac)
-        for svc in dev.services:
+        self.dev = btle.Peripheral(self.__server_mac)
+        print('Services on server  ------------------')
+        for svc in self.dev.services:
             print(str(svc))
 
- 
+    
+
+    def connect_to_server(self):
+        print('Connect to server--------------------' )
+        #        self.p = Peripheral(self.__server_mac, addrType=ADDR_TYPE_PUBLIC)
+        #self.p = Peripheral(self.__server_mac)
+        self.dev.setDelegate(MyDelegate())
+        print('Connected') 
+
+
+
+    def update_charactoreristic(self, new_value):
+        pass
+    
+    def spin(self):
+        while True:
+            pass
+
 if __name__ =='__main__':
     print('practice BLE')
     runner = ble()
     runner.scan()
+    runner.list_services_on_server()
+    runner.connect_to_server()
+    runner.spin()
 
 
 
@@ -38,27 +66,27 @@ if __name__ =='__main__':
 
 #for svc in dev.services:
 #    print (str(svc))
-
-lightSensor = btle.UUID("f000aa70-0451-4000-b000-000000000000")
+if False:
+    lightSensor = btle.UUID("f000aa70-0451-4000-b000-000000000000")
  
-lightService = dev.getServiceByUUID(lightSensor)
-for ch in lightService.getCharacteristics():
-     print (str(ch))
+    lightService = dev.getServiceByUUID(lightSensor)
+    for ch in lightService.getCharacteristics():
+         print (str(ch))
 
-import time
-import binascii
-uuidConfig = btle.UUID("f000aa72-0451-4000-b000-000000000000")
-lightSensorConfig = lightService.getCharacteristics(uuidConfig)[0]
-# Enable the sensor
-lightSensorConfig.write(bytes("\x01"))
+    import time
+    import binascii
+    uuidConfig = btle.UUID("f000aa72-0451-4000-b000-000000000000")
+    lightSensorConfig = lightService.getCharacteristics(uuidConfig)[0]
+    # Enable the sensor
+    lightSensorConfig.write(bytes("\x01"))
 
-time.sleep(1.0) # Allow sensor to stabilise
+    time.sleep(1.0) # Allow sensor to stabilise
  
-uuidValue  = btle.UUID("f000aa71-0451-4000-b000-000000000000")
-lightSensorValue = lightService.getCharacteristics(uuidValue)[0]
-# Read the sensor
-val = lightSensorValue.read()
-print ("Light sensor raw value", binascii.b2a_hex(val))
+    uuidValue  = btle.UUID("f000aa71-0451-4000-b000-000000000000")
+    lightSensorValue = lightService.getCharacteristics(uuidValue)[0]
+    # Read the sensor
+    val = lightSensorValue.read()
+    print ("Light sensor raw value", binascii.b2a_hex(val))
 
 
 
