ea2852dbd9d42d686e4c41e57d58b19bcec12ee2
Xuming <voicevon@gmail.com>
2021-06-24 22:09:28 +0800

    add mcp23018

--- esp32_arm/src/accel_stepper/AccelStepper.h
+++ esp32_arm/src/accel_stepper/AccelStepper.h
@@ -93,7 +93,7 @@ public:
 	DRIVER    = 1, ///< Stepper Driver, 2 driver pins required
 	FULL2WIRE = 2, ///< 2 wire stepper, 2 motor pins required
 	FULL3WIRE = 3, ///< 3 wire stepper, such as HDD spindle, 3 motor pins required
-        FULL4WIRE = 4, ///< 4 wire full stepper, 4 motor pins required
+    FULL4WIRE = 4, ///< 4 wire full stepper, 4 motor pins required
 	HALF3WIRE = 6, ///< 3 wire half stepper, such as HDD spindle, 3 motor pins required
 	HALF4WIRE = 8  ///< 4 wire half stepper, 4 motor pins required
     } MotorInterfaceType;
