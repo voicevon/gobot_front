90450510cc62c9215bd3707698e85d7497832ea1
Xuming <voicevon@gmail.com>
2021-06-27 17:55:30 +0800

    revision

--- gogame/died_area_scanner.py
+++ gogame/died_area_scanner.py
@@ -1,23 +1,11 @@
-import logging
-import time
 
-
-from chessboard_cell import ChessboardCell
-import sys
-sys.path.append("/home/pi/pylib")
-from terminal_font import TerminalFont
-# from mqtt_helper import g_mqtt
-
-
-class DiedAreaScanner():
-
+class DiedAreaScanner(ChessboardLayout):
     def __init__(self):
-        self.__target_color_code = 3
+        ChessboardLayout.__init__(self,'Died area scanner')
         self.__died_area_array = [([0] * 19) for i in range(19)]
 
-        self._FC_YELLOW = TerminalFont.Color.Fore.yellow
-        self._BG_GREEN = TerminalFont.Color.Background.green
-        self._FC_RESET = TerminalFont.Color.Control.reset
+        # self.__DIED_BLACK = self._BLACK + 100
+        # self.__DIED_WHITE = self._WHITE + 100
 
     def __is_around_alived(self, col, row):
         '''
@@ -30,12 +18,12 @@ class DiedAreaScanner():
         '''
 
         # look up,down,left,right. 
-        if row > 1:
+        if row > 0:
             s = self.__died_area_array[col][row - 1]
             if s > 11:
                 # upper cell is blank
                 return s + 1
-        if col > 1:
+        if col > 0:
             s = self.__died_area_array[col - 1][row]
             if s > 11:
                 # left cell is blank
@@ -68,8 +56,7 @@ class DiedAreaScanner():
 
         return 0
 
-        
-    def backword_loop(self):
+    def __backword_loop(self):
         c = 0
         for col in range(18,-1,-1):
             for row in range(18,-1,-1):
@@ -83,7 +70,7 @@ class DiedAreaScanner():
                         self.__died_area_array[col][row] = wake_up_type 
         return c
 
-    def forward_loop(self):
+    def __forward_loop(self):
         count = 0
         for col in range(0,19):
             for row in range(0,19):
@@ -97,46 +84,76 @@ class DiedAreaScanner():
                         self.__died_area_array[col][row] = wake_up_type
         return count
 
-    def start(self, layout_array, target_color_code):
+    def start_scan(self, target_color):
         '''
+        return:
+            count: How many cells are died
+
         # 0 = assume cell is died
         # 1 = No need to be waken up, because this cell is opposit color
         # 15 = has been wakup up, because this cell is blank
         '''
-        self.__init_died_area_array(layout_array, self.__target_color_code)
+        self.__init_died_area_array(self._layout_array, target_color)
         # print('inited')
         # self.print_out()
 
         count = 9999
         while count > 0:
             count = 0 
-            count += self.forward_loop()
+            count += self.__forward_loop()
             # self.print_out()
-            count += self.backword_loop()
+            count += self.__backword_loop()
             # self.print_out()
-        return self.__died_area_array
 
-    def print_out(self):
+        count = 0
+        for col in range(0, self._COLS):
+            for row in range(0, self._ROWS):
+                if self.__died_area_array[col][row] == 0:
+                    count += 1
+        return count
+
+    def print_out_died_area(self):
         int_to_char = {0:'x ', 1:'O ', 15:'. ', 16:'- ', 17:'* ', 8:'B ', 3:'W '}
-        print()
-        print()
-        print(self.__FC_YELLOW + 'Died area')
+        print(self._FC_YELLOW + 'Died area')
         cell = ChessboardCell()
         # print column name on table head
         header = '    '
         for col_id in range (19,-1,-1):
             cell.from_col_row_id(col_id=col_id, row_id=1)
             header += cell.col_letter + ' '
-        print(self.__FC_YELLOW + header)
+        print(self._FC_YELLOW + header)
         # print layout row by row
         for row_id in range(0, 19):
             rowNum = 19 - row_id
             col_string = ''
             for col_id in range(0,19):
-                col_string += int_to_char[self.__died_area_array[18 - col_id][18 - row_id]]
+                value = self.__died_area_array[18 - col_id][18 - row_id]
+                if value == 0:
+                    col_string += self._BG_RED + int_to_char[value] + self._FC_RESET
+                else:
+                    col_string += int_to_char[value]
+
             row_string = "%02d" % rowNum + '  '
-            print (self.__FC_YELLOW + row_string + self.__FC_RESET + col_string + self.__FC_YELLOW + row_string)
-        print(self.__FC_YELLOW + header)
+            print (self._FC_YELLOW + row_string + self._FC_RESET + col_string + self._FC_YELLOW + row_string)
+        print(self._FC_YELLOW + header)
+
+    def get_first_died_cell(self):
+        '''
+        return:
+            None means not found!
+        '''
+        cell = ChessboardCell()
+        for row_id in range(0, self._ROWS):
+            for col_id in range(0, self._COLS):
+                if self.__died_area_array[col_id][row_id] == 0:
+                    cell.from_col_row_id(col_id,row_id)
+                    return cell
+        return None
+
+    def died_cell_removed_first_one(self):
+        cell = ChessboardCell()
+        cell = self.get_first_died_cell()
+        self.__died_area_array[cell.col_id][cell.row_id] = 15
 
     def __init_died_area_array(self, origin_array,target_color_code):
         '''
@@ -162,5 +179,5 @@ class DiedAreaScanner():
                     # oppsite color
                     self.__died_area_array[col][row] = 1
 
-if __name__ == '__main__':
-    t = DiedAreaScanner()
+    def set_layout_array(self,layout_array):
+        self._layout_array = layout_array
