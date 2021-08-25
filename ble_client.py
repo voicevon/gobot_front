b375171460cbcccba519d738599985be22c9b3ca
Xuming <voicevon@gmail.com>
2021-08-25 18:28:39 +0800

    scan

--- ble_client.py
+++ ble_client.py
@@ -1,3 +1,7 @@
+  334  quit
+  335  exit
+  GNU nano 3.2                                                                                                                      ble_client.py                                                                                                                                 
+
 from bluepy import btle  
 '''
 sudo apt-get install libglib2.0-dev
@@ -13,34 +17,64 @@ https://github.com/hbldh/bleak
 import time
 import logging
 
+# Instruction:
+#    sudo python3 ble_client.py
+#
+#
+
 # https://anthonychiu.xyz/2016/04/05/communication-between-raspberry-pi-and-multiple-arduinos-via-bluetooth-low-power-ble/
 
 class MyDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print ('Disovered device',dev.addr)
+        if isNewData:
+            print('Received new data from ', dev.addr)
+
     def handleNotification(self, cHandle, data):
         print('Notification is invoked')
         
+#scanner = Scanner().withDelegate(MyDelegate())
+#devices = scanner.scan(10.0)
+#for dev in devices:
+#    print('Devide %s (%s), RSSI=%d DB' %(dev.addr, dev.addrType, dev.rssi))
+#    for(adtype, desc, value) in dev.getScanData():
+#       print('%s = %s'%(desc, value))
 
 class BleClient():
-    def __init__(self, mac_addr):
-        self.__arm_mac = 'b4:e6:2d:b2:f8:8f'
-        self.__house_mac = '12:34:56:78:90:ab'
+    def __init__(self,gobot_id):
+        self.__gobot_id = gobot_id
+        #self.__arm_mac = 'b4:e6:2d:b2:f8:8f'
+        #self.__house_mac = '12:34:56:78:90:ab'
         # self.scan()
 
         ##self.dev = btle.Peripheral(self.__server_mac)
         # self.list_services_on_server()
-        self.connect_to_server()
+        #self.connect_to_server()
         # self.dev.
 
-    def scan(self):
+
+    def scan_arm_house(self):
         scanner = Scanner()
-        devices = scanner.scan(timeout = 3)
+        devices = scanner.scan(timeout = 8)
         print('Scan result:  Found %d devices in %d seconds------------' % (len(devices), 3))
         for dev in devices:
-            print('Name', dev.getValueText(9))
-            print('Address:', dev.addr)
+            name = dev.getValueText(9)
+            print('Server Name', name)
+            print('       Mac Address:', dev.addr)
+            if name == 'ConInt-Arm-' + self.__gobot_id:
+                logging.info('Discoverd Arm !')
+                self.__arm_mac_addr = dev.addr
+                self.__dev_arm = btle.Peripheral(dev.addr)
+                self.connect_to_arm()
+
+                logging.info('Connected Arm !')
+            if name == 'ConInt-House-' + self.__gobot_id:
+                self.__house_mac_addr = dev.addr
+                logging.info('Discovered House !')
 
     def list_services_on_server(self, server_mac):
         logging.info('Services on server  ------------------')
@@ -51,11 +85,16 @@ class BleClient():
     
 
     def connect_to_arm(self):
-        self.arm.withDelegate(MyDelegate())
-        svc = self.arm.getServiceByUUID('4fafc201-1fb5-459e-8fcc-c5c9c331914b')
-        self.arm_info = svc.getCharacteristics('beb5483e-36e1-4688-b7f5-ea07361b26a8')[0]
+        self.__dev_arm.withDelegate(MyDelegate())
+        logging.info('1111111111111111111111111111111111111111111111111')
+        svc = self.__dev_arm.getServiceByUUID('20210825')
+        #service = self.__dev_arm.getServiceByUUID('')
+        logging.info('------222222222222222222222')
+        self.arm_info = svc.getCharacteristics('20210826')[0]
         #self.house_info = svc.getCharacteristics('beb5483e-36e1-4688-b7f5-ea07361b26a8')[1]
         logging.info('ble connected to GATT server Arm !')
+
+
 
     def connect_to_house(self):
         self.house.withDelegate(MyDelegate())
@@ -98,13 +137,14 @@ class BleClient():
         self.dev.disconnect()
         time.sleep(1)
 
-g_bleClient = BleClient()
+g_bleClient = BleClient('213401')
 
 if __name__ == '__main__':
     import signal        
     import sys
     logging.basicConfig(level=logging.DEBUG)
     logging.info('@@@@@@@@@@@@@@@@@@@@@@@@@')
+    g_bleClient.scan_arm_house()
     # runner = BleClient()
     def signal_handler(sig, frame):
         print("You pressed Ctrl+C")
@@ -122,15 +162,9 @@ if __name__ == '__main__':
     #logging.basicConfig(level=logging.DEBUG)
     logging.info('practice BLE')
     #runner = BleClient()
-    while True:
+    while False:
         time.sleep(1.7)
         i += 1
         if i> 128:
             i = 32
         g_bleClient.write_characteristic([i])
-
-
-
-
-
-
