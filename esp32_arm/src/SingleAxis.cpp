fc26904ac04c55c2a944d97fc86bd940f2e37df6
Xuming <voicevon@gmail.com>
2021-09-15 01:14:35 +0800

    reconstruct

--- esp32_arm/src/SingleAxis.cpp
+++ esp32_arm/src/SingleAxis.cpp
@@ -1,5 +1,6 @@
 #include "SingleAxis.h"
 
+
 #define PIN_HOME_SENSOR 5
 #define PIN_ENCODER_A 6
 #define PIN_ENCODER_B 7
@@ -12,41 +13,80 @@ void SingleAxis::SetTargetAbs(int targetPosition){
 
 }
 
-SingleAxis::SingleAxis(DCMotor* motor, Encoder* encoder){
+
+SingleAxis::SingleAxis(){
+}
+
+void SingleAxis::Init(DCDriverHBridge* _driver, Encoder* _encoder, uint8_t pin_home){
+  driver = _driver;
+  encoder = _encoder;
+
+  motor = new DCMotor();
+  motor->linkDriver(driver);
   motor->linkSensor(encoder);
-  motor->controller = MotionControlType::angle;
-  
-}
 
-SingleAxis::SingleAxis(uint8_t pinA, uint8_t pinB, uint8_t pin_home_sensor){
-    __pinA = pinA;
-    __pinB = pinB;
-    __pinHomeSensor = pin_home_sensor;
-    pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
-    pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
-}
-
-void SingleAxis::Setup(void (*ISR_callback)(void)){
-    attachInterrupt(digitalPinToInterrupt(__pinA), ISR_callback, RISING);
-    // attachInterrupt(digitalPinToInterrupt(3),&SingleAxis::ISR_Encoder, 0);
-    attachInterrupt(5,  [this](void)ICACHE_RAM_ATTR{ encoderPos++; },RISING); 
-    attachInterrupt(5,  [this](void)ICACHE_FLASH_ATTR{ encoderPos++; },RISING); 
-    //https://github.com/esp8266/Arduino/issues/7788#issuecomment-750364982
-    // attachInterrupt(6,this->ISR_Encoder,FALLING );
-}
-void SingleAxis::setup2(std::function<void(void)> callback){
-    attachInterrupt(digitalPinToInterrupt(__pinA), callback, RISING);
+  __pinHomeSensor = pin_home;
 
 }
-void(*ISR_callback)();
+void SingleAxis::Init(uint8_t motor_pinA, uint8_t motor_pinB, uint8_t encoder_pinA, uint8_t encoder_pinB, uint8_t pin_home ){
+  driver= new DCDriverHBridge(motor_pinA, motor_pinB);
+  encoder = new Encoder(encoder_pinA,encoder_pinB,200); 
 
+  motor = new DCMotor();
+  motor->linkDriver(driver);
+  motor->linkSensor(encoder);
 
-void ICACHE_RAM_ATTR SingleAxis::ISR_Encoder(void){
-  cli(); //stop interrupts happening before we read pin values
-  if(digitalRead(__pinB)) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
-    encoderPos--; //decrement the encoder's position count
-  }
-  else
-    encoderPos++;
-  sei(); //restart interrupts
+  __pinHomeSensor = pin_home;
 }
+
+
+void SingleAxis::Move(float distanceRel){
+
+}
+
+void SingleAxis::MoveAsync(){
+
+}
+
+void SingleAxis::Home(){
+
+}
+
+// SingleAxis::SingleAxis(DCMotor* motor, Encoder* encoder){
+//   motor->linkSensor(encoder);
+//   motor->controller = MotionControlType::angle;
+
+// }
+
+// SingleAxis::SingleAxis(uint8_t pinA, uint8_t pinB, uint8_t pin_home_sensor){
+//     __pinA = pinA;
+//     __pinB = pinB;
+//     __pinHomeSensor = pin_home_sensor;
+//     pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+//     pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+// }
+
+// void SingleAxis::Setup(void (*ISR_callback)(void)){
+//     attachInterrupt(digitalPinToInterrupt(__pinA), ISR_callback, RISING);
+//     // attachInterrupt(digitalPinToInterrupt(3),&SingleAxis::ISR_Encoder, 0);
+//     attachInterrupt(5,  [this](void)ICACHE_RAM_ATTR{ encoderPos++; },RISING); 
+//     attachInterrupt(5,  [this](void)ICACHE_FLASH_ATTR{ encoderPos++; },RISING); 
+//     //https://github.com/esp8266/Arduino/issues/7788#issuecomment-750364982
+//     // attachInterrupt(6,this->ISR_Encoder,FALLING );
+// }
+// void SingleAxis::setup2(std::function<void(void)> callback){
+//     attachInterrupt(digitalPinToInterrupt(__pinA), callback, RISING);
+
+// }
+// void(*ISR_callback)();
+
+
+// void ICACHE_RAM_ATTR SingleAxis::ISR_Encoder(void){
+//   cli(); //stop interrupts happening before we read pin values
+//   if(digitalRead(__pinB)) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+//     encoderPos--; //decrement the encoder's position count
+//   }
+//   else
+//     encoderPos++;
+//   sei(); //restart interrupts
+// }
--- esp32_arm/src/SingleAxis.h
+++ esp32_arm/src/SingleAxis.h
@@ -9,22 +9,37 @@
 
 class SingleAxis{
     public:
-        SingleAxis(uint8_t pinA, uint8_t pinB, uint8_t pin_home_sensor);
-        SingleAxis(DCMotor* motor, Encoder* encoder);
-        void SetTargetAbs(int targetPosition);
-        void Setup(void (*ISR_callback)(void));
+        // SingleAxis(uint8_t pinA, uint8_t pinB, uint8_t pin_home_sensor);
+        // SingleAxis(DCMotor* motor, Encoder* encoder);
+        SingleAxis();
+        DCMotor* motor;
+        Encoder* encoder;
+        DCDriver* driver;
 
-        void setup2(std::function<void(void)> callback);
-        void ICACHE_RAM_ATTR ISR_Encoder(void);
-        void(*ISR_callback)();
+        // uint8_t Pin_home_sensor;
+
+        void Init(DCDriverHBridge* driver, Encoder* encoder, uint8_t pin_home);
+        void Init(uint8_t motor_pinA, uint8_t motor_pinB, uint8_t encoder_pinA, uint8_t encoder_pinB, uint8_t pin_home );
+        void SetTargetAbs(int targetPosition);
+        void Move(float distanceRel);
+        void MoveAsync();
+        void Home();
+        // void Setup(void (*ISR_callback)(void));
+
+        // void setup2(std::function<void(void)> callback);
+        // void ICACHE_RAM_ATTR ISR_Encoder(void);
+        // void(*ISR_callback)();
     private:
         // static void InteruptPinB();
-        uint8_t __pinA; // Our first hardware interrupt pin is digital pin 2
-        uint8_t __pinB; // Our second hardware interrupt pin is digital pin 3
+        // uint8_t __pinA; // Our first hardware interrupt pin is digital pin 2
+        // uint8_t __pinB; // Our second hardware interrupt pin is digital pin 3
         uint8_t __pinHomeSensor;
+        float home_position;
+        float distance_per_motor_encoder;
+
         // static volatile byte aFlag; // let's us know when we're expecting a rising edge on pinA to signal that the encoder has arrived at a detent
         // static volatile byte bFlag; // let's us know when we're expecting a rising edge on pinB to signal that the encoder has arrived at a detent (opposite direction to when aFlag is set)
-        volatile int32_t encoderPos ; //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
+        // volatile int32_t encoderPos ; //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
         // static volatile byte oldEncPos ; //stores the last encoder position value so we can compare to the current reading and see if it has changed (so we know when to print to the serial monitor)
         // static volatile byte reading ; //somewhere to store the direct values we read from our interrupt pins before checking to see if we have moved a whole detent
 
