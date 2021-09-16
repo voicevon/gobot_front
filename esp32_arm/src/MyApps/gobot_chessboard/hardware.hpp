6eb39f5392ac471c6505a340295366dc1c4f4671
Xuming <voicevon@gmail.com>
2021-09-17 02:13:04 +0800

    remove the family of board  class

---
+++ esp32_arm/src/MyApps/gobot_chessboard/hardware.hpp
@@ -0,0 +1,26 @@
+#include "all_devices.h"
+#ifdef I_AM_GOBOT_CHESSBOARD
+
+
+
+#define LED_A 12
+#define LED_B 25
+#define LED_C 26
+#define LED_F 27
+#define LED_H 14
+
+#define PIN_HOME_ALHPA 35     //??
+#define PIN_ALPHA_DIR 19
+#define PIN_ALPHA_STEP 5
+#define PIN_ALPHA_ENABLE 18
+
+#define PIN_HOME_BETA 34      //??
+#define PIN_BETA_DIR 17
+#define PIN_BETA_STEP 4
+#define PIN_BETA_ENABLE 16
+
+#define ENDER_COIL 32
+#define ENDER_COIL_EXT 33
+
+
+#endif
---
+++ esp32_arm/src/MyApps/gobot_house/hardware.hpp
@@ -0,0 +1,31 @@
+#ifndef __GOBOT_HOUSE_HPP_
+#define __GOBOT_HOUSE_HPP_
+
+#define PIN_LED_A 12
+#define PIN_LED_B 25
+#define PIN_LED_C 26
+#define PIN_LED_F 27
+#define PIN_LED_H 14
+
+#define PIN_HOME_ALHPA 35     //??
+#define PIN_ALPHA_DIR 19
+#define PIN_ALPHA_STEP 5
+#define PIN_ALPHA_ENABLE 18
+
+#define PIN_HOME_BETA 34      //??
+#define PIN_BETA_DIR 17
+#define PIN_BETA_STEP 4
+#define PIN_BETA_ENABLE 16
+
+#define ENDER_COIL 32
+#define ENDER_COIL_EXT 33
+
+
+#include "ESP32Step/src/TeensyStep.h"
+
+
+
+
+
+
+#endif
