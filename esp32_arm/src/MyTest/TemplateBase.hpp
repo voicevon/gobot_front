2a5eac9c1cdb12dc9df5669c88c1877b64698fc2
Xuming <voicevon@gmail.com>
2021-09-16 21:39:12 +0800

    Learned template  compler - linking.

---
+++ esp32_arm/src/MyTest/TemplateBase.hpp
@@ -0,0 +1,15 @@
+#ifndef _TEMPLATE_BASE_HPP_
+#define _TEMPLATE_BASE_HPP_
+
+#include "TemplateBase.h"
+
+
+//  What do we learned?
+//      DO NOT put below code to any of *.cpp file.
+
+template <int nnnn>
+void Base<nnnn>::bget(int x){
+    // return N;
+}
+
+#endif
