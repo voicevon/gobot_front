6d5394cc3216b5f7cc600a94487be389b62a4ac9
Xuming <voicevon@gmail.com>
2021-09-27 14:46:33 +0800

    return nothing was wrong

--- config/config_cable_bot_center.py
+++ config/config_cable_bot_center.py
@@ -14,6 +14,7 @@ class CornerBotFactory:
         head.BleServiceUUID = "1fd03a14-1451-11ec-82a8-0242ac130003"
         head.BleCommuUUID = "4b7fbe96-1451-11ec-82a8-0242ac130003"
         head.BleStateUUID = "50711238-1451-11ec-82a8-0242ac130003"
+        return head
 
     def CreateCorner_XNYP() -> BleServerHead:
         head = BleServerHead()
@@ -22,6 +23,7 @@ class CornerBotFactory:
         head.BleServiceUUID = "6909288a-1451-11ec-82a8-0242ac130003"
         head.BleCommuUUID = "6e0d5d7e-1451-11ec-82a8-0242ac130003"
         head.BleStateUUID = "72838810-1451-11ec-82a8-0242ac130003"
+        return head
 
     # class XNYN:
     def CreateCorner_XNYN() -> BleServerHead:
@@ -31,6 +33,7 @@ class CornerBotFactory:
         head.BleServiceUUID = "77d37e60-1451-11ec-82a8-0242ac130003"
         head.BleCommuUUID = "8fdb27b0-1451-11ec-82a8-0242ac130003"
         head.BleStateUUID = "8175103c-1451-11ec-82a8-0242ac130003"
+        return head
 
     # class XPYN:
     def CreateCorner_XPYN() -> BleServerHead:
@@ -40,3 +43,4 @@ class CornerBotFactory:
         head.BleServiceUUID  = "85f5ab94-1451-11ec-82a8-0242ac130003"
         head.BleCommuUUID = "96a1cd38-1451-11ec-82a8-0242ac130003"
         head.BleStateUUID  = "9c43b29c-1451-11ec-82a8-0242ac130003"
+        return head
