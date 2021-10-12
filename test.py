ed0cbf1a3a009529bd8a1e1e4b5eb5c65c9f6803
Xuming <voicevon@gmail.com>
2021-10-12 13:00:59 +0800

    test 6050

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
-response = 'trigger = Yes'
-print(response)
-print(response[-3:])
+
+# sudo pip3 install adafruit-circuitpython-mpu6050
+
+import time
+import board
+import adafruit_mpu6050
+
+i2c = board.I2C()
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print("Temperature: %.2f C" % mpu.temperature)
+    print("")
+    time.sleep(1)
+    
