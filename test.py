a0a49c5d747670f0f70b5dc8a78fbe05e7112e45
Xuming Feng <voicevon@gmail.com>
2022-02-09 08:59:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ while(True):
     ret, frame = vid.read()
   
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imwrite('frame', frame)
       
     # the 'q' button is set as the
     # quitting button you may use any
