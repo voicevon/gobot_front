b70b7ffe2153d38e563bde2fe7744bfba4610576
Xuming <voicevon@gmail.com>
2022-02-10 20:12:57 +0800

    more test method

--- vision/robot_eye_usb_camera.py
+++ vision/robot_eye_usb_camera.py
@@ -1,18 +1,20 @@
 
+import sys
+sys.path.append('C:\\gitlab\\gobot_front')  # For runing in VsCode on Windows-10 
 from vision.robot_eye_base import MonoEyeBase
 import cv2
 
 class MonoEyeUsbCamera(MonoEyeBase):
     def __init__(self, coefficients_file):
-        self.__camera = cv2.VideoCapture(0)
+        self.__camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     def take_picture(self, do_undistort=True):
         return_value, image = self.__camera.read()
         if do_undistort:
-            if self.__show_debug_info:
+            if super.__show_debug_info:
                 print('RobotEye.take_picture()   start undistortion')
             undistort_image = cv2.undistort(image, self.__mtx, self.__dist, None, None)
-            if self.__show_debug_info:
+            if super.__show_debug_info:
                 print('...... end undistortion')
             return undistort_image
         return image
@@ -27,9 +29,18 @@ class MonoEyeUsbCamera(MonoEyeBase):
 if __name__ == '__main__':
 
     my_eye = MonoEyeUsbCamera('2021-0611.yml')
-    image = my_eye.take_picture(do_undistort=False)
-    cv2.imshow("Captured", image)
-    cv2.waitKey(5000)
+    while False:
+        # Video test
+        image = my_eye.take_picture(do_undistort=False)
+        cv2.imshow("Captured", image)
+        cv2.waitKey(1)
+
+    while True:
+        # Video test. When Ctrl+C, there is an error
+        image = my_eye.take_picture(do_undistort=False)
+        cv2.imshow("Captured", image)
+        cv2.waitKey(5000)
+
     if False:
         my_eye.take_batch_picture_for_calibration()
         my_eye.recalibrate_and_save_coefficients()
