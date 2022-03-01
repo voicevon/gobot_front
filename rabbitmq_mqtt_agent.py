6bf3cbd4dabbc2ccd0267adae3d2638c45b3dbef
Xuming <voicevon@gmail.com>
2022-03-01 16:13:26 +0800

    single channel can stop all cosuming now.

--- rabbitmq_mqtt_agent.py
+++ rabbitmq_mqtt_agent.py
@@ -16,6 +16,11 @@ class RabbitMQAgent:
         self.a = None
         self.b = None
 
+        '''
+        Only for goboy_x2134_house
+        '''
+        self.consuming_message_in_queue = True
+
 
     def callback(self, ch, method, properties, body):
         print('[x] Received ' ,  method.routing_key, body)
@@ -24,6 +29,11 @@ class RabbitMQAgent:
             # a new command from gobot_head is received
             g_mqtt.publish('gobot/x2134/house/', body)
             #stop consume a
+            print("Stop consuming now..")
+            # self.channel.stop_consuming()
+            self.consuming_message_in_queue = False
+
+
 
         if method.routing_key == 'gobot.x2134.house.fb':
             self.b = body
@@ -31,7 +41,8 @@ class RabbitMQAgent:
                 # feedback is equal to last command.
                 # gobot-house has received last message in the gobot_head command queue.
                 # go on to comsume a
-                pass
+                self.consuming_message_in_queue = True
+                print("Start consuming now..")
 
     def SubsribeRabbitMQ(self, config:RabbitMQConfig):
         credentials = pika.PlainCredentials(config.uid, config.password)
@@ -39,13 +50,18 @@ class RabbitMQAgent:
                                         port= config.port,
                                         virtual_host= config.virtual_host,
                                         credentials= credentials)
-        connection = pika.BlockingConnection(parameters)
-        channel = connection.channel()
-        channel.queue_declare(queue='gobot_x2134_house')
-        channel.basic_consume(queue='gobot_x2134_house', on_message_callback=self.callback, auto_ack=True )
-        channel.queue_declare(queue='gobot_x2134_house_fb')
-        channel.basic_consume(queue='gobot_x2134_house_fb', on_message_callback=self.callback, auto_ack=True )
-        channel.start_consuming()
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='gobot_x2134_house')
+        self.channel.basic_consume(queue='gobot_x2134_house', on_message_callback=self.callback, auto_ack=True )
+        self.channel.queue_declare(queue='gobot_x2134_house_fb')
+        self.channel.basic_consume(queue='gobot_x2134_house_fb', on_message_callback=self.callback, auto_ack=True )
+        # channel.start_consuming()
+        while True:
+            if self.consuming_message_in_queue:
+                if self.channel._consumer_infos:
+                    self.channel.connection.process_data_events(time_limit=1)
+            # print(".")
 
 if __name__ == '__main__':
     config_mqtt = MQTT_ConnectionConfig()
