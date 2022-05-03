fa8af1073b3cc5bcd8b054a249c925602d48a0b6
Xuming Feng <voicevon@gmail.com>
2022-05-03 19:20:26 +0800

    arm map is unified to robot_map

---
+++ gogame/arm_map.py
@@ -0,0 +1,56 @@
+import enum
+
+from Pylib.robot_map import MapSite
+from gogame.chessboard_cell import ChessboardCell
+
+
+class ArmMapSite_Catalog(enum.Enum):
+    TRASH_BIN_BLACK = 1,
+    TRASH_BIN_WHITE = 2,
+    HOUSE_VENDOR = 3,
+    CHESSBOARD_CELL = 4,
+    PARKING = 5
+
+class ArmMapSites():
+    def __init__(self) -> None:
+        self.house_vendor = MapSite('house_vendor', 0, 25)
+        self.trash_bin_white = MapSite('trash_bin_white', -230, 0)
+        self.trash_bin_black = MapSite('trash_bin_black', 230, 0)
+        self.parking = MapSite('Parking', 0, 25)
+
+    def GetSingleSite(self, cat:ArmMapSite_Catalog, cell:ChessboardCell=None) -> MapSite:
+        '''
+        TODO:  static or class method.  What different?
+        '''
+        if cat == ArmMapSite_Catalog.HOUSE_VENDOR:
+            return self.house_vendor
+        elif cat == ArmMapSite_Catalog.TRASH_BIN_WHITE:
+            return self.trash_bin_white
+        elif cat == ArmMapSite_Catalog.TRASH_BIN_BLACK:
+            return self.trash_bin_black
+        elif cat == ArmMapSite_Catalog.CHESSBOARD_CELL:
+            x, y = self.__Get_XY_FromCell(cell)
+            cell_site = MapSite("Q1", x, y)
+            return cell_site
+        else:
+            print("[Error] ArmMapSites.GetSingleSite()  cat=, index= ", cat, cell)
+
+
+    def __Get_XY_FromCell(self, cell:ChessboardCell):
+        '''
+        Go_position instance: "Q4" as position on 2D Chessboard.
+        XY_position instance: "[0.015,0.02,-0.01]" as [x,y,z] in 3D world coordinator.
+        '''
+        # ss = "ABCDEFGHIJKLMNOPQRST"
+        # print(cell.name)
+        # iCol = ss.find(cell_name[:1])
+        # iRow = 19 - int(cell_name[1:])
+        zero_in_world_x = -215
+        zero_in_world_y = 155
+        cell_width_x = 22
+        cell_width_y = 23
+        x = zero_in_world_x +  cell_width_x * cell.col_id
+        y = zero_in_world_y +  cell_width_y * cell.row_id
+        return x, y
+
+
--- gogame/house_map.py
+++ gogame/house_map.py
@@ -1,8 +1,6 @@
 import enum
 
-from collections import namedtuple
-from logging import exception
-MapSite = namedtuple("MapSite",['Name','X','Y'])
+from Pylib.robot_map import MapSite
 
 class HouseMapSite_Catalog(enum.Enum):
     ROOM = 1,
@@ -13,29 +11,29 @@ class HouseMapSite_Catalog(enum.Enum):
 
 class HouseMapSites():
     def __init__(self) -> None:
-        self.head = MapSite('Head', 20,30)
-        self.neck = MapSite('Neck',123,234)
-        self.parking = MapSite('Parking',22,34)
+        self.head = MapSite('Head', 148.93, 0)
+        self.neck = MapSite('Neck', 55.0, 0)
+        self.parking = MapSite('Parking', 55.0, -2)
 
         self.doors = [MapSite]
-        self.doors.append (MapSite('Door_0',122,333))
-        self.doors.append (MapSite('Door_1',122,333))
-        self.doors.append (MapSite('Door_2',122,333))
-        self.doors.append (MapSite('Door_3',122,333))
-        self.doors.append (MapSite('Door_4',122,333))
-        self.doors.append (MapSite('Door_5',122,333))
-        self.doors.append (MapSite('Door_6',122,333))
-        self.doors.append (MapSite('Door_7',122,333))
+        self.doors.append (MapSite('Door_0',-10.37,  59.07))
+        self.doors.append (MapSite('Door_1',-21.39, 56.12))
+        self.doors.append (MapSite('Door_2',-44.28, 40.67))
+        self.doors.append (MapSite('Door_3',-57.42, 17.95))
+        self.doors.append (MapSite('Door_4',-57.42, -17.95))
+        self.doors.append (MapSite('Door_5',-44.28, -40.67))
+        self.doors.append (MapSite('Door_6',-21.39, -56.12))
+        self.doors.append (MapSite('Door_7',-10.37, -59.07))
         
         self.rooms = [MapSite]
-        self.rooms.append(MapSite('Room_0', 22,33))
-        self.rooms.append(MapSite('Room_1', 22,33))
-        self.rooms.append(MapSite('Room_2', 22,33))
-        self.rooms.append(MapSite('Room_3', 22,33))
-        self.rooms.append(MapSite('Room_4', 22,33))
-        self.rooms.append(MapSite('Room_5', 22,33))
-        self.rooms.append(MapSite('Room_6', 22,33))
-        self.rooms.append(MapSite('Room_7', 22,33))
+        self.rooms.append(MapSite('Room_0', -78.79, 126.38))
+        self.rooms.append(MapSite('Room_1', -98.18, 95.3))
+        self.rooms.append(MapSite('Room_2', -111.9, 61.4))
+        self.rooms.append(MapSite('Room_3', -119.51, 25.59))
+        self.rooms.append(MapSite('Room_4', -119.51, -25.59))
+        self.rooms.append(MapSite('Room_5', -111.9, -61.4))
+        self.rooms.append(MapSite('Room_6', -98.18, -95.2))
+        self.rooms.append(MapSite('Room_7', -78.79, -126.38))
 
     def GetSingleSite(self, cat:HouseMapSite_Catalog, index:int) -> MapSite:
         if cat == HouseMapSite_Catalog.HEAD:
@@ -51,33 +49,4 @@ class HouseMapSites():
         else:
             print("[Error] HouseMapSites.GetSingleSite()  cat=, index= ", cat, index)
 
-
-
-
-class HouseMapDiction():
-
-    def __init__(self) -> None:
-        self.NECK = (55.0, 0)
-        self.HEAD = (148.93, 0)
-        self.PARKING = (55.0, -2)
-
-        self.DOORS = [(-10.37,  59.07),
-                (-21.39, 56.12),
-                (-44.28, 40.67),
-                (-57.42, 17.95),
-                (-57.42, -17.95),
-                (-44.28, -40.67),
-                (-21.39, -56.12),
-                (-10.37, -59.07),
-                ]
-
-        self.ROOMS  = [(-78.79,  126.38),  #0
-                (-98.18, 95.3),              #1
-                (-111.9, 61.4),              #2
-                (-119.51, 25.59),            #3
-                (-119.51, -25.59),           #4
-                (-111.9, -61.4),             #5
-                (-98.18, -95.2),             #6
-                (-78.79, -126.38)            #7
-                ]
 
