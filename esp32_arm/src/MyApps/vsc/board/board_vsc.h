e4a5d1bb84638c01d16f8756f36bba5b02646c4b
wx225117 <1398375529@qq.com>
2022-06-23 19:07:23 +0800

    dc motor basic methods

--- esp32_arm/src/MyApps/vsc/board/board_vsc.h
+++ esp32_arm/src/MyApps/vsc/board/board_vsc.h
@@ -1,8 +1,6 @@
 #pragma once
-// #include "../board_base.h"
-// #include "../cnc_board_base.h"
 #include "MyBoards/cnc_board_base.h"
-#include "AGV/mover_driver/single_wheel_driver/single_wheel_h_bridge_pwm_driver.h"
+#include "CNC/Actuator/dc_motor/actuator_dc_motor.h"
 #include <SimpleFOC.h>
 #include "CNC/single_axis_homer.h"
 
@@ -16,14 +14,14 @@ class Vsc_Board: public CncBoardBase{
         Vsc_Board(){};
         void Init(bool is_on_reset) override{};
         void SayHello() override {};
-        SingleWheel_HBridgePwmDriver* GetDcMotor(){return &this->__motor;};
+        ActuatorDcMotor* GetDcMotor(){return &this->__motor;};
         MagneticSensorAnalog* GetSensor(){return &this->__sensor;};
         SingleAxisHomer* GetHomer(char axis_name){return &this->__homer; };
 
     protected:
 
     private:
-	    SingleWheel_HBridgePwmDriver __motor = SingleWheel_HBridgePwmDriver(PIN_H_BRIDGE_A, PIN_H_BRIDGE_B);
+	    ActuatorDcMotor __motor = ActuatorDcMotor(PIN_H_BRIDGE_A, PIN_H_BRIDGE_B);
         MagneticSensorAnalog __sensor = MagneticSensorAnalog(PIN_SENSOR_ADC);
         SingleAxisHomer __homer = SingleAxisHomer(PIN_HOMER_SENSOR_HALL, LOW);
 
--- esp32_arm/src/MyApps/vsc/main.cpp
+++ esp32_arm/src/MyApps/vsc/main.cpp
@@ -14,18 +14,33 @@ MessageQueue mqtt_command_queue;
 void test_board(){
     Vsc_BoardTest tester;
     tester.LinkBoard(&board);
-    tester.Test_AngleSensor(100);
-
-    tester.Test_AllHomers(81);
-    // tester.Test_Motor(20);
+    tester.Test_Offset(5);
+    tester.Test_AngleSensor(0);
+    tester.Test_AllHomers(0);
+    tester.Test_Motor();
 
 }
+
+void home(){
+    SingleAxisHomer* homer = board.GetHomer('A');
+    bool trigered = false;
+    ActuatorDcMotor* motor = board.GetDcMotor();
+    motor->SetSpeed(100);
+    motor->SetTargetPositionTo(false, 999999);
+    motor->StartToMove();
+    while (trigered){
+        // motor->SpinOnce();
+        motor->Stop();
+        motor->SetCurrentPositionAs(15);
+    }
+}
+
 void setup(){
     board.Init(true);
     test_board();
     // robot.LinkLocalGcodeQueue_AsProducer(&gcode_queue);
     // cnc.LinkLocalGcodeQueue_AsConsumer(&gcode_queue);
-
+    home();
     setup_mqtt_block_connect();
     append_mqtt_bridge("spring/maker", &mqtt_command_queue, &robot); 
     setup_mqtt_on_message_receive(); 
@@ -34,6 +49,7 @@ void setup(){
 }
 
 void loop(){
+    board.GetDcMotor()->SpinOnce();
     robot.SpinOnce();
     // cnc.SpinOnce();
     loop_mqtt();
