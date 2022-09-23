aeb190b42eac85a5827b4272c639dfe56a421eb9
Xuming Feng <voicevon@gmail.com>
2022-09-23 16:07:31 +0800

    minus

--- esp32_arm/src/MyLibs/pid_controllers/pid_controllers.cpp
+++ esp32_arm/src/MyLibs/pid_controllers/pid_controllers.cpp
@@ -8,7 +8,7 @@ PidControllers::PidControllers(uint8_t pid_controller_count){
 void PidControllers::AppendPidController(PIDController* new_pid_controller){
     this->__all_pid_controllers[this->__next_controller_index] = new_pid_controller;
     this->__next_controller_index++;
-    if (this->__next_controller_index >= this->__max_controller_index){
+    if (this->__next_controller_index > this->__max_controller_index){
         Logger::Error("PidControllers::AppendPidController()   TOO MANY CONTROLLERS INSIDE!");
     }
 }
