6a1681a15d6202e7b89d72939245de7150378643
Xuming Feng <voicevon@gmail.com>
2022-09-24 19:27:45 +0800

    minus

--- esp32_arm/src/MyApps/vsc/main.cpp
+++ esp32_arm/src/MyApps/vsc/main.cpp
@@ -27,7 +27,7 @@ void test_board(){
     // tester.Test_AngleSensor();
     // tester.Test_EncoderSensor(&encoder, 0);
     // tester.Test_SinglePositionTrigger(board.GetSingleHomer(AXIS_ALPHA), 0);
-    tester.Test_AxisHomer(board.GetCncHomers()->GetAxisHomer(AXIS_ALPHA), 0);
+    // tester.Test_AxisHomer(board.GetCncHomers()->GetAxisHomer(AXIS_ALPHA), 0);
 
     tester.Test_MotorDriver(board.GetActuator(AXIS_ALPHA), 0);
     tester.Test_Offset(0);
