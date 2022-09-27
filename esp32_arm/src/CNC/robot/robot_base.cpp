0fda8481308879dbba526fd784e2747b30cd7762
Xuming Feng <voicevon@gmail.com>
2022-09-27 08:30:22 +0800

    prepare robot

---
+++ esp32_arm/src/CNC/robot/robot_base.cpp
@@ -0,0 +1,340 @@
+#include "robot_base.h"
+#include "MyLibs/MyFunctions.hpp"
+#include "HardwareSerial.h"
+#include "MyLibs/message_queue.h"
+
+
+void RobotBase::SayHello(){
+	Serial.println("[Debug] RobotBase::SayHello()");
+}
+
+void RobotBase::SpinOnce(){
+	// Logger::Debug("RobotBase::SpinOnce()");
+	// Logger::Print("this->State", this->State);
+	switch (this->State){
+	case CncState::IDLE:   //0
+		break;
+	case CncState::RUNNING_G4:  // 2
+		this->__running_G4();
+		break;
+	case CncState::RUNNING_G1:  // 1
+		this->_running_G1();
+		break;
+	case CncState::RUNNING_G28:  // 4
+		// Logger::Debug("CncState::RUNNING_G28");
+		this->_running_G28();
+		break;
+	default:
+		Logger::Warn("RobotBase::SpinOnce() Unknown current state: ");
+		break;
+	}
+
+	// Serial.println("[Debug]( RobotBase::SpinOnce() is finished.)");
+	this->SpinOnce_BaseExit();
+}
+
+// Check gcode queue, if there is gcode to be run.
+void RobotBase::SpinOnce_BaseExit(){
+	if (this->State != CncState::IDLE)
+		return;
+	if (this->_gcode_queue->BufferIsEmpty())
+		return;
+
+	MessageQueue::SingleMessage* message = this->_gcode_queue->FetchTailMessage();
+	if (message == NULL){
+		Serial.println("\n\n\n [Error] RobotBase::SpinOnce_BaseExit() tail_message is null. \n\n ");
+		return;
+	}
+
+	bool debug = false;
+	if (debug){
+		Logger::Debug("[Info] RobotBase::SpinOnce_BaseExit()  Going to run next gcode   ===> ");
+		Serial.print(message->payload);
+		Serial.println(" ");
+	}
+	
+	// type convert   from char* to std::string
+	char* p = &message->payload[0];
+	std::string str = std::string(p);
+	// feed std::string to Gcode constructor.
+	Gcode gcode = Gcode(str);
+	this->RunGcode(&gcode);
+}
+
+
+
+
+//Can deal with:  home via single actuator.
+//Can NOT deal with:  CoreXY, It's combined moving.
+void RobotBase::RunG28(EnumAxis axis){ 
+	bool debug = true;
+	if (debug){
+		Logger::Debug("RobotBase::RunG28() is entering" );
+		Logger::Print("axis", axis);
+		Logger::Print("IsCombinedFK", this->_config_base.IsCombinedFk);
+	}
+
+	if (this->_config_base.IsCombinedFk){
+		this->_RunG28_CombinedFk(axis);
+
+	}else{
+		this->__HomeSingleAxis(axis);
+	}
+}
+
+
+void RobotBase::__HomeSingleAxis(EnumAxis axis){
+	Logger::Debug("RobotBase::__HomeSingleAxis()");
+	this->_homing_axis = axis;
+	HomingConfig* homing = this->_cnc_homer.GetAxisHomer(axis)->GetHomingConfig();
+	this->_cnc_board->EnableMotor(axis, true);
+	
+	this->_config_base.PrintOut("RobotBase::__HomeSingleAxis()  _config_base");
+	this->_mover_base->PrintOut("RobotBase::__HomeSingleAxis()  _mover_base" );
+	this->_mover_base->SetActuatorSpeed(axis, homing->Speed);
+	this->_mover_base->SetActuatorAcceleration(axis, homing->Accelleration);
+	// this->_mover_base->SingleActuatorMoveTo(axis, false, homing->DistanceToGo);
+	LineSegment line;
+	line.axis = axis;
+	line.IsAbsTargetPosition = false;
+	line.TargetPosition = homing->DistanceToGo;
+	line.Speed = homing->Speed;
+	this->_mover_base->SingleActuatorMoveTo(&line);   //TOdo:  Put this line to line_queue
+
+	Logger::Debug("RobotBase::RunG28() is Starting to run..." );
+}
+
+void RobotBase::_running_G28(){
+	bool debug = false;
+	if(debug){
+		Logger::Debug("RobotBase::_running_G28() is entering...");
+		Logger::Print("_homing_axis", this->_homing_axis);
+		auto homer = this->_cnc_homer.GetAxisHomer(this->_homing_axis);
+		Logger::Print("Got axis_homer",true);
+		auto index = homer->GetTrigeredIndex();
+		Logger::Print("Got triggered index", index);
+	}
+
+	int fired_trigger_index =  this->_cnc_homer.GetAxisHomer(this->_homing_axis)->GetTrigeredIndex();
+	if (fired_trigger_index >=0 ){
+		// End stop is trigered
+		Logger::Info("RobotBase::_running_G28() ----> Home sensor is triggered." );
+		Logger::Print("_homing_axis_name", this->_homing_axis);
+		Logger::Print(" fired_trigger_index", fired_trigger_index);
+
+		this->_mover_base->AllActuatorsStop();
+		this->_SetCurrentPositionAsHome(this->_homing_axis);
+		this->State = CncState::IDLE;
+	}else{
+		// Endstop is not trigered
+		// Serial.print(".");
+		delay(10);
+	}
+}
+
+void RobotBase::_running_G1(){
+	if (this->_mover_base->HasArrivedTargetPosition()){
+	// float distance_to_target = this->GetDistanceToTarget_IK();
+    // if (distance_to_target < DEG_TO_RAD * 1.0f){  // TODO:  == 0  or decide by subclass.
+
+      	this->State = CncState::IDLE;
+		Logger::Info("RobotBase::_running_G1() is finished. ");
+		// Logger::Print("distance_to_target", RAD_TO_DEG * distance_to_target);
+    }
+}
+
+void RobotBase::RunG4(Gcode* gcode){
+	__g4_start_timestamp = micros();
+	__g4_time_second = gcode->get_value('S');
+}
+
+void RobotBase::__running_G4(){
+	long delayed = (micros() - __g4_start_timestamp) / 1000 /1000;
+	if (delayed >= __g4_time_second ){
+		this->State = CncState::IDLE;
+		return;
+	}
+}
+
+void RobotBase::RunM123(uint8_t eef_channel, uint8_t eef_action){
+	Logger::Debug("RobotBase::RunM123()");
+	Logger::Print("eef_action", eef_action);
+	// uint8_t action_code = 1;
+	this->__eef->PrintOut();
+	this->__eef->Run(eef_action);
+}
+
+void RobotBase::RunM84(){
+	//TODO: CNC_AXIS_COUNT_IK,   vs CNC_AXIS_COUNT_FK
+	for (int axis=0; axis<CNC_AXIS_COUNT; axis++){
+		this->_cnc_board->EnableMotor(EnumAxis(axis), false);
+	}
+}
+void RobotBase::RunGcode(Gcode* gcode){
+	std::string result;
+	// if ((gcode->get_command() == COMMU_OK) || (gcode->get_command() == COMMU_UNKNOWN_COMMAND)){
+	//   Serial.print("RunGcode()   OK or Unknown");
+	//   return;
+	// }
+	bool debug = false;
+	Logger::Debug("RobotBase::RunGcode()");
+	Logger::Print("gcode_command", gcode->get_command());
+
+	if(gcode->has_g){
+		char home_axis = '+';
+		switch (gcode->g){
+		case 28:
+			// G28: Home
+			this->State = CncState::RUNNING_G28;
+			if (gcode->has_letter('X')) home_axis='X';
+			if (gcode->has_letter('Y')) home_axis='Y';
+			if (gcode->has_letter('Z')) home_axis='Z';
+			if (gcode->has_letter('A')) home_axis='A';
+			if (gcode->has_letter('B')) home_axis='B';
+			if (gcode->has_letter('C')) home_axis='C';
+			if (gcode->has_letter('W')) home_axis='W';
+
+			// Is there any machine that supports both IK, and FK homing?
+			// this->_home_via_inverse_kinematic = false;
+			if (home_axis == '+'){
+				Serial.print("\n\n\n\n[Error] RobotBase::RunGcode()  :");
+				Serial.print(home_axis);
+
+			}
+			//TODO:  convert char to enum
+			this->RunG28(this->ConvertToEnum(home_axis));
+			// this->commuDevice->OutputMessage(COMMU_OK);  For calble-bot-corner, it should be 'Unknown Command'
+			break;
+
+		case 1:
+			// G1 Move
+			//TODO:  1. put position to movement queue. called "plan" in smoothieware? 
+			//       2. send out OK.
+			//       3. Set status to busy.
+			//       4. Start Moving.
+			this->State = CncState::RUNNING_G1;
+			this->RunG1(gcode);
+			// this->commuDevice->OutputMessage(COMMU_OK);
+			break;
+		case 4:
+			// G4 Dwell, Pause for a period of time.
+			this->State = CncState::RUNNING_G4;
+			this->RunG4(gcode);
+			break;
+		case 6:
+			this->RunG6(gcode);
+			// this->commuDevice->OutputMessage(COMMU_OK);
+			break;
+		case 90:
+			// Absolute position
+			this->is_absolute_position = true;
+			// this->commuDevice->OutputMessage(COMMU_OK);
+			break;
+		case 91:
+			// Relative position
+			this->is_absolute_position = false;
+			// this->commuDevice->OutputMessage(COMMU_OK);
+			break;
+		// case 92:
+			// Set Position     G92 X10 E90
+			// break;
+		default:
+			break;
+		}
+	}else if(gcode->has_m){
+		uint8_t p_value = 33;   //TODO: Make sure this is no harmful!
+		uint8_t s_value = 0;
+		float f_value = 0.0f;
+		EefAction action;
+		switch (gcode->m){
+		case 42:
+			p_value =  gcode->get_value('P');
+			s_value = gcode->get_value('S');
+			this->Run_M42_OutputGpio(p_value, s_value);
+		case 84:
+			this->RunM84();
+		case 114:
+			// Get Current Position
+			break;
+
+		case 119:
+			// Get Endstop Status
+			result = GetHomeTrigerStateString();
+			// this->commuDevice->OutputMessage(COMMU_OK);
+			Serial.print(result.c_str());
+			// this->commuDevice->WriteNotification(result);
+			break;
+
+		case 123:
+			//M123 P=channel_index, S=Set EEF action			
+			while (this->State != CncState::IDLE){
+				this->SpinOnce();
+			}
+			p_value =  gcode->get_value('P');
+			s_value = gcode->get_value('S');
+			debug = true;
+			if (debug){
+				Logger::Debug("RobotBase::RunGcode() For EEF_ACTION  M123 ");
+				Logger::Print("P", p_value);
+				Logger::Print("S", s_value);
+			}
+			action = (EefAction)s_value;
+			this->RunM123(p_value, s_value);
+			break;
+
+		case 130:
+			Logger::Debug("RobotBase::RunGcode()   M130");
+			Logger::Print("gcode", gcode->get_command());
+			p_value =  gcode->get_value('N');
+			Logger::Print("Index", p_value);
+			
+			f_value = gcode->get_value('P');
+			this->__pid_controllers->GetController(p_value)->P = f_value;
+			Logger::Print("P", f_value);
+
+			f_value = gcode->get_value('I');
+			this->__pid_controllers->GetController(p_value)->I = f_value;
+			Logger::Print("I", f_value);
+
+			f_value = gcode->get_value('D');
+			this->__pid_controllers->GetController(p_value)->D = f_value;
+			Logger::Print("D", f_value);
+
+			break;
+
+		case 141:
+			break;
+
+		case 280:
+			// Set servo position  
+			//	 	Pnnn Servo index
+			// 		Snnn Angle or microseconds
+			// Wait for all gcode, mcode is finished
+			// Serial.println("M280 Started");
+			while (this->State != CncState::IDLE){
+				this->SpinOnce();
+			}
+			if (gcode->has_letter('P')) p_value = gcode->get_value('P');
+			if (gcode->has_letter('S')) s_value = gcode->get_value('S');
+			ledcWrite(p_value, s_value);   // from ledcWrite(ledChannel, dutyCycle);
+			// this->commuDevice->OutputMessage(COMMU_OK);
+			// this->commuDevice->WriteNotification("IDLE");
+			break;
+		case 996:
+			// Do nothing. this should be the last gcode of a movement in transaction.
+			// after MCU reset, This should be the first gcode it received, even the message queue is nothing.
+			break;
+		default:
+			break;
+		}
+	}else{
+		// this->commuDevice->OutputMessage("\n[Warning] RobotBase::RunGcode()  Has NO letter 'G' or 'M'. ");
+		// this->commuDevice->OutputMessage(gcode->get_command());
+		// this->commuDevice->OutputMessage(COMMU_UNKNOWN_COMMAND);
+	}
+}
+
+
+void RobotBase::Run_M42_OutputGpio(uint8_t pin_number, uint8_t pin_value){
+	digitalWrite(pin_number, pin_value);
+}
---
+++ esp32_arm/src/CNC/robot/robot_base.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "Robot/eef/eef_standard_code.h"
+#include "CNC/gcode/gcode_consumer.h"
+#include "CNC/board/cnc_board_base.h"
+
+#include "CNC/arm_solution/kinematic_config.h"
+#include "CNC/arm_solution/axis_homer/axis_homer.h"
+#include "../coordinate/coordinate_base.h"
+#include "../coordinate/cnc_axis.h"
+#include "../mover/cnc_mover_base.h"
+#include "MyLibs/pid_controllers/pid_controllers.h"
+#include "CNC/gcode/line_segment_queue.h"
+
+enum class CncState{
+    IDLE,
+    RUNNING_G1,
+    RUNNING_G4,
+    RUNNING_G28
+};
+
+class RobotBase: public GcodeConsumer{
+    public:
+        CncState State = CncState::IDLE;
+        void SpinOnce();
+
+        void RunGcode(Gcode* gcode);
+        void RunG28(EnumAxis axis);
+        virtual bool GetCurrentPosition(FkPositionBase* position_fk);
+        virtual float GetDistanceToTarget_FK();
+        virtual float GetDistanceToTarget_IK();
+        void SayHello();    // TODO:  be virtual
+
+    protected:
+        // virtual _DispatchGcode(Gcode* gcode);
+        void _LinkEef(RobotEefBase* eef){this->__eef=eef;};
+        void _LinkPidControllers(PidControllers* pid_controllers){this->__pid_controllers=pid_controllers;};
+       
+        void SpinOnce_BaseExit();
+        virtual void IK(FkPositionBase* from_fk, IkPositionBase* to_ik);
+        virtual void FK(IkPositionBase* from_ik,FkPositionBase* to_fk);
+        virtual void RunG1(Gcode* gcode);   //None blocking, move backgroundly.
+        virtual void RunG6(Gcode* gcode);   //Block mode
+        void RunG4(Gcode* gcode);
+        virtual std::string GetHomeTrigerStateString();
+
+        void Run_M42_OutputGpio(uint8_t pin_number, uint8_t pin_value);
+        
+        void RunM84();
+        // virtual void RunM123(uint8_t eef_channel, EefAction eef_action);
+        void RunM123(uint8_t eef_channel, uint8_t eef_action);
+
+        void _running_G1();
+        void _running_G28();
+        virtual EnumAxis ConvertToEnum(char axis);
+        virtual void _SetCurrentPositionAsHome(EnumAxis homing_axis);
+		virtual void _RunG28_CombinedFk(EnumAxis axis){};
+
+        EnumAxis _homing_axis;
+        KinematicConfig _config_base;    //TODO:  rename to _kinamatic_config
+        CncHomers _cnc_homer = CncHomers(CNC_AXIS_COUNT);
+
+        bool is_absolute_position = true;
+        
+        CncBoardBase* _cnc_board;
+        CncMoverBase* _mover_base;   //???
+        LineSegmentQueue* _line_segment_queue;
+
+
+    private:
+        int test_int;
+        RobotEefBase* __eef;
+        // Why it's a pointer, not a object? Because I don't know how many pid controllers in the system. I can't init it.
+        // And the author doesn't want to use "new PidControll()"  to create an instance.
+        PidControllers* __pid_controllers;   
+        void _base_spin_once();
+        void __running_G4();
+        void __HomeSingleAxis(EnumAxis axis);
+        long __g4_start_timestamp;
+        int __g4_time_second;
+
+        /* Just for fun, don't remove below comment.
+        void * __output_message2;
+        void (* __output_message3); 
+        void OnFinishedGcode2(void(*callback)()) {__output_message2 = callback;};
+        void OnFinishedGcode3(void(*callback)()) {__output_message2 = callback;};
+        void OnFinishedGcode4(void(*callback())) {__output_message3 = callback;};
+        void OnFinishedGcode5(void(*callback)()) {__output_message3 = callback;};
+        */
+
+};
+
