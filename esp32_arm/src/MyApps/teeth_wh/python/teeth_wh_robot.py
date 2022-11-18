3c59425112b758d6f0c41f32f7375645c34f6015
Xuming Feng <voicevon@gmail.com>
2022-11-18 20:30:00 +0800

    amq reading

--- esp32_arm/src/MyApps/teeth_wh/python/teeth_wh_robot.py
+++ esp32_arm/src/MyApps/teeth_wh/python/teeth_wh_robot.py
@@ -1,29 +1,10 @@
+from von.rabbitmq_agent import RabbitMqAgent, AMQ_BrokerConfig
+import json
 
 
-class WarehouseLocation:
-    row =1
-    col =2
 
-class Message_TwhRequest():
-    is_withdraw = True
-    locations = [WarehouseLocation()]
 
-    def test(self):
-        self.locations[1].row
 
-class Message_TwhGcode():
-    pass
-
-class AmqAgent():
-    def __init__(self) -> None:
-        pass
-
-    def FetchMessage(self) -> Message_TwhRequest:
-        request = Message_TwhRequest()
-        return request
-
-    def SendMessage(self, gcode_message):
-        pass
 
 class GcodeSender():
 
@@ -34,29 +15,36 @@ class GcodeSender():
         '''
         For Ir checking product
         '''
+        print('gcode ','EefMove_Z_toTop')
         pass
 
     def EefMove_Z_toMiddle(self, depth:int):
         '''
         For 
         '''
+        print('gcode ','EefMove_Z_toMiddle')
         pass
 
-    def Eef_TurnOn_VacuumFan(self, is_turn_on:bool):
+    def Eef_TurnOn_VacuumFan(self, is_turn_on: bool):
+        print('gcode ','Eef_TurnOn_VacuumFan')
         pass
 
     def Eef_EnableSuck(self, is_enable:bool):
+        print('gcode ','Eef_EnableSuck')
         pass
 
     def PauseForWaiting(self, second: int):
+        print('gcode ','PauseForWaiting')
         pass
 
 
     def MoveArmToCenter(self):
+        print('gcode ','MoveArmToCenter')
         g1 = "G1A0"
 
 
     def MoveTo(self, x, y):
+        print('gcode ','MoveTo')
         g1 = "G!X" + str(x) + "Y" + str(y)
 
 
@@ -66,14 +54,14 @@ class RobotCerebellum():
         self.gcode_sender = GcodeSender()
         pass
 
-    def GetXY_FromRowCol(self, location: WarehouseLocation):
-        x = location.row * 40
-        y = location.col * 40
+    def GetXY_FromRowCol(self, row, col):
+        x = row * 40
+        y = col * 40
         return x,y
 
-    def PickupAndCheck(self, location: WarehouseLocation):
+    def PickupAndCheck(self, row: int, col: int):
         self.gcode_sender.EefMove_Z_toTop()
-        x,y = self.GetXY_FromRowCol(location)
+        x,y = self.GetXY_FromRowCol(row=row, col=col)
         self.gcode_sender.MoveTo(x,y)
         self.gcode_sender.EefMove_Z_toMiddle(12)
         self.gcode_sender.Eef_EnableSuck(True)
@@ -89,31 +77,34 @@ class TeethWarehouseRobot():
     def __init__(self) -> None:
         self.cerebellum = RobotCerebellum()
 
-    def ExecCommand(self, request: Message_TwhRequest) -> bool:
+    def ExecCommand(self, command: str) -> bool:
         '''
         This should start a new thread.
         Reason: Dynamic gcode follow a  branched processing flow. Synced with hardware robot. 
                 And take a long time up to couple minutes.
         '''
-        if request.is_withdraw:
-            index = 0
-            while index <request.locations.count():
+        print(command)
+        if command == '"withdraw_end"':
+            print('withdraw_end')
+        else:
+            request = json.loads(command)
+            # while index <request.locations.count():
                 # clear message on broker.
-                pickup_state = 'todo'
-                while pickup_state != 'done':
-                    if pickup_state == 'todo':
-                        self.cerebellum.PickupAndCheck(request.locations[index].row, request.locations[index].col)
-                        pickup_state = 'doing'
-                    if pickup_state == 'doing':
-                        #check message, and update suck_state
-                        if (1==1):
-                            # got message:  ir is blocked
-                            pickup_state = 'done'
-                        if (2==3):
-                            # got message:  ir is passing through.
-                            pickup_state = 'todo'
+            pickup_state = 'todo'
+            while pickup_state != 'done':
+                if pickup_state == 'todo':
+                    self.cerebellum.PickupAndCheck(request['row'], request['col'])
+                    pickup_state = 'doing'
+                if pickup_state == 'doing':
+                    #check message, and update suck_state
+                    if (1==1):
+                        # got message:  ir is blocked
+                        pickup_state = 'done'
+                    if (2==3):
+                        # got message:  ir is passing through.
+                        pickup_state = 'todo'
 
-                self.cerebellum.Dropto_CenterBox()
+            self.cerebellum.Dropto_CenterBox()
                 # checking weight before-dropping
             
                 # # checking weight after-dropping
@@ -123,14 +114,32 @@ class TeethWarehouseRobot():
                 # if (False):
                 #     # Weight is increased too much.
                 #     pass
-                index += 1
+                # index += 1
 
             # All the location in request is done. 
             # Now, send a message to inform user:  "Your request is dealed. Please pick up the bag."
 
+robot = TeethWarehouseRobot()
+
+def callback_example_twh( ch, method, properties, body):
+        # print('RabbitMqAgent::callback_example()  mq Received ' ,  method.routing_key, body)
+        # channel.basic_ack(delivery_tag=method.delivery_tag)
+        robot.ExecCommand(body.decode("utf-8") )
+        # if body == 'withdraw_end':
+        #     print('withdraw_end')
+        # else:
+        #     msg = json.loads(body)
+        #     print(msg)
+
+
 if __name__ == '__main__':
-    robot = TeethWarehouseRobot()
+    g_amq = RabbitMqAgent()
+    amq_broke_config = AMQ_BrokerConfig()
+    g_amq.connect_to_broker(amq_broke_config)
+    g_amq.Subscribe("twh_221109_request",callback=callback_example_twh)
+    
     while True:
-        request = AmqAgent.FetchMessage()
-        if request is not None:
-            robot.ExecCommand(request)
+        # request = AmqAgent.FetchMessage()
+        # g_amq.connect_to_broker(amq_broke_config)
+        g_amq.SpinOnce()
+
