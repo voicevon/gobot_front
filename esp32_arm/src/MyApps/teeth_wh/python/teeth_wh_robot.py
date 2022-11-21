8b9d10237d39b46ac4cf3869af396a128c05ab0c
Xuming Feng <voicevon@gmail.com>
2022-11-21 13:55:24 +0800

    amq and statemachine

--- esp32_arm/src/MyApps/teeth_wh/python/teeth_wh_robot.py
+++ esp32_arm/src/MyApps/teeth_wh/python/teeth_wh_robot.py
@@ -76,11 +76,16 @@ class RobotCerebellum():
 
 class StateMachine_Item():
 
-    def __init__(self, current_state:str, command:str, post_state:str, service_function) -> None:
-        self.command = command
+    def __init__(self, current_state:str, command:str, post_state:str, service_function, repeat_serve=False) -> None:
         self.current_state = current_state
+        self.command = command
         self.post_state = post_state
         self.service_function = service_function
+        self.repeat_invoke_service_function = repeat_serve
+
+    def print_out(self):
+        print("------------ StateMachine_Item() -------------")
+        print("from_state=", self.current_state, "  command=", self.command, "    post_state=", self.post_state)
 
     @classmethod
     def from_json(cls, json_string):
@@ -91,6 +96,8 @@ class StateMachine_Item():
         if current_state in self.current_state:
             return True
         return False
+
+
 
     # def update(self):
     #     self.current_state = self.ne
@@ -109,51 +116,78 @@ class TeethWarehouseRobot():
     def init_statemachine(self):
         # self.current_state = 'idle'
         self.machine = [StateMachine_Item('idle', 'prepare_deposit', 'preparing_deposit', self.do_prepare_deposit)]
-        # self.machine.append(StateMachine_Item('idle', 'prepare_withdraw', 'preparing_withdraw', self.do_prepare_withdraw))
-        # self.machine.append(StateMachine_Item('preparing_withdraw', 'cancel', 'idle', self.do_cancel))
         self.machine.append(StateMachine_Item('preparing_deposit', 'cancel', 'idle', self.do_cancel))
+        self.machine.append(StateMachine_Item('preparing_deposit', 'start_deposit', 'depositting', self.do_cancel))
         # self.machine.append(StateMachine_Item('preparing_deposit', 'cancel', 'idle', self.do_cancel))
+
+        self.machine.append(StateMachine_Item('idle', 'pickup', 'picking_cell', self.do_pickup))
+        self.machine.append(StateMachine_Item('picking_cell', 'ir_check_pass', 'idle', self.do_ir_check, repeat_serve=True))
+        self.machine.append(StateMachine_Item('picking_cell', 'ir_check_failed', 'picking_cell', self.do_pickup))
+        self.machine.append(StateMachine_Item('preparing_withdraw', 'withdrawing', '', self.do_cancel))
 
         self.current_state = 'idle'
 
 
     def is_acceptable(self, command) ->bool:
         for item in self.machine:
+            # item.print_out()
             if item.current_state == self.current_state:
                 if item.command == command:
                     return True
         return False
 
-    def update_state(self, command:str) -> bool:
+    def statemachine_drive(self, command:str, house_cell) -> bool:
         '''
         Returns
         True:  Update to post state successed.
         False: Update to post state failed.
         '''
         for item in self.machine:
+            item.print_out()
             if item.current_state == self.current_state:
                 if item.command == command:
-                    item.service_function()
+                    item.service_function(house_cell)
                     self.current_state = item.post_state
                     return True
         print("TeethWarehouseRobot::update_state() failed")
         return False 
 
-    def do_prepare_deposit(self):
+    def do_prepare_deposit(self, house_cell):
         pass
     
-    def do_prepare_withdraw(self):
+    def do_pickup(self, house_cell):
+        #get x,y from row, col
+        # gcode move to x,y
+        # mcode ir check
+        self.ir_state.local_value = self.ir_state.default_value
+
+    def do_cancel(self, house_cell):
         pass
 
-    def do_cancel(self):
-        self.current_state = 'idle'
-        pass
+    def do_ir_check(self, house_cell):
+        match self.ir_state.remote_value:
+            case 'unknown':
+                # Have not get new state
+                return
+
+            case "passed":
+                self.statemachine_drive('ir_check_pass')
+
+            case "failed":
+                self.statemachine_drive('ir_check_failed')
+
+    def do_deposite_user_ready(self, house_cell):
+        self.cerebellum.Dropto_CenterBox()
+        self.cerebellum.PickupAndCheck(house_cell)
+
 
     def SpinOnce(self):
-        pass
+        if self.current_state == 'picking_cell':
+            self.do_ir_check(self.house_cell)
+        
 
     
-    def AppendCommand(self, request_str: str) -> bool:
+    def feed_request(self, request_str: str) -> bool:
         '''
         This should start a new thread.
         Reason: Dynamic gcode follow a  branched processing flow. Synced with hardware robot. 
@@ -164,14 +198,45 @@ class TeethWarehouseRobot():
         '''
         request = json.loads(request_str)
         command = request['command']
+        self.house_cell = request['cell']
         # if request.Acceptable():
-        self.update_state(command)
+        print('-------------------', request,command)
+        return self.statemachine_drive(command, self.house_cell)
 
+def test():
 
+    g_amq = RabbitMqAgent()
+    amq_broke_config = AMQ_BrokerConfig()
+    g_amq.connect_to_broker(amq_broke_config)
 
+    # img = cv2.imread("nocommand.jpg")
+    # g_amq.publish_cv_image("test" , img)
 
+    g_amq.Subscribe(queue_name='twh_221109_deposit')
+    g_amq.Subscribe(queue_name='twh_221109_withdraw')
+    count = 0
+    while True:
+        g_amq.SpinOnce()
+        # time.sleep(0.9)
+        # xx = g_amq.fetch_message('twh_221109_deposit')
+        # # xx = ss.FetchMessage()
+        # if xx is not None:
+        #     print(xx)
+        #     # time.sleep(1)
+
+        xx = g_amq.fetch_message('twh_221109_withdraw')
+        if xx is not None:
+            if count>1:
+                print("withdraw is empty................................", count)
+            count =0
+            print(xx)
+            # time.sleep(1)
+        else:
+            count += 1
 
 if __name__ == '__main__':
+    test()
+
     g_mqtt.connect_to_broker(g_mqtt_broker_config)
     robot = TeethWarehouseRobot()
 
@@ -180,15 +245,38 @@ if __name__ == '__main__':
         g_amq = RabbitMqAgent()
         amq_broke_config = AMQ_BrokerConfig()
         g_amq.connect_to_broker(amq_broke_config)
-        g_amq.Subscribe("twh_221109_request")
-        
+        g_amq.Subscribe("twh_221109_deposit")
+        g_amq.Subscribe('twh_221109_withdraw')
+
         while True:
-            g_amq.SpinOnce()
-            user_request = g_amq.FetchMessage()
-            if user_request is not None:
-                robot_accepted_request = False
-                while not robot_accepted_request:
-                    robot.AppendCommand(user_request.decode('utf-8'))
-                    time.sleep(0.1) 
+            deal_deposit = True
+            while deal_deposit:
+                g_amq.SpinOnce()
+                user_request = g_amq.fetch_message('twh_221109_deposit')
+                if user_request is None:
+                    print('deposit is empty ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+                    time.sleep(2)
+                    deal_deposit = False
+                # else:
+                #     robot_accepted_request = False
+                    # while not robot_accepted_request:
+                    #     robot_accepted_request = robot.feed_request(user_request.decode('utf-8'))
+                    #     if not robot_accepted_request:
+                    #         time.sleep(1) 
+            while not deal_deposit:
+                g_amq.SpinOnce()
+                user_request = g_amq.fetch_message('twh_221109_withdraw')
+                if user_request is None:
+                    # print('withdraw is empty ==============================================================================')
+                    # time.sleep(2)
+                    # deal_deposit = True
+                    pass
+                else:
+                #     robot_accepted_request = False
+                    print(user_request)
+                    # while not robot_accepted_request:
+                        # robot_accepted_request = robot.feed_request(user_request.decode('utf-8'))
+                        # if not robot_accepted_request:
+                        #     time.sleep(1) 
             
 
