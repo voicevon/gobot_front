5a7cbc81cc6d944f768a0fab3a6062c3db400efd
Xuming Feng <voicevon@gmail.com>
2022-12-11 11:23:07 +0800

    Update tesla_checker.py

--- tesla_checker.py
+++ tesla_checker.py
@@ -2,14 +2,24 @@
 import cv2
 
 # intialize the webcam and pass a constant which is 0
-cam = cv2.VideoCapture(1)
-
+cam = cv2.VideoCapture(0)
+# cam.set(3, 1280) # set the resolution
+# cam.set(4, 720)
+# cam.set(cv2.CAP_PROP_AUTOFOCUS, 0) # turn the autofocus off
+# cam.set(cv2.CAP_PROP_BRIGHTNESS,250)
+# cam.set(cv2.CAP_PROP_CONTRAST,0)
+# cam.set(cv2.CAP_PROP_SATURATION, 255)
+# cam.set(cv2.CAP_PROP_SETTINGS, 0)
 # title of the app
 cv2.namedWindow('python webcam screenshot app')
 
 # let's assume the number of images gotten is 0
 img_counter = 0
-
+upper_y=150
+lower_y= 270
+height = 100
+left = 70
+width = 500
 # while loop
 while True:
     # intializing the frame, ret
@@ -20,6 +30,10 @@ while True:
         break
     # the frame will show with the title of test
     cv2.imshow('test', frame)
+    upper = frame[upper_y:upper_y + height, left:left + width]
+    lower = frame[lower_y:lower_y + height, left:left + width]
+    cv2.imshow('upper',upper)
+    cv2.imshow('lower',lower)
     #to get continuous live video feed from my laptops webcam
     k  = cv2.waitKey(1)
     # if the escape key is been pressed, the app will stop
