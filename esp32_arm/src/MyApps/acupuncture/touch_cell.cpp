37c3c3d8ccb88696a930c6c4312d2a87549ecf27
Xuming Feng <voicevon@gmail.com>
2022-12-31 23:11:04 +0800

    optimize has_update()

--- esp32_arm/src/MyApps/acupuncture/touch_cell.cpp
+++ esp32_arm/src/MyApps/acupuncture/touch_cell.cpp
@@ -1,5 +1,5 @@
 #include "touch_cell.h"
-#include "HardwareSerial.h"
+#include "MyLibs/basic/logger.h"
 
 #define PPD2 0
 #define PPD3 1
@@ -21,7 +21,13 @@
 
 
 bool TouchCell::HasUpdate(){
-    return this->_has_update;
+    bool has_update = false;
+    for(int i=0; i<4; i++){
+        if (LastFlags[i] != CurrentFlags[i]){
+            has_update = true;
+        }
+    }
+    return has_update;
 }
 
 const char* TouchCell::GetName(int point_id){
@@ -34,31 +40,37 @@ const char* TouchCell::GetName(int point_id){
 }
 
 
-bool TouchCell::CompareCurrentAndLast(){
-    for(int i=0; i<4; i++){
-        if (LastFlags[i] != CurrentFlags[i]){
-            Serial.println("Some touch pad is changed.");
-            this->_has_update = true;
-            return true;   
-        }
-    }
-    this->_has_update = false;
-    return false;
-}
+// bool TouchCell::CompareCurrentAndLast(){
+
+//     return _has_update;
+// }
 
 void TouchCell::CopyCurrentToLast(){
     for(int i =0; i<4; i++){
         this->LastFlags[i] = this->CurrentFlags[i];
     }
 }
+
+// paras: bit_index=[0..15]
 bool TouchCell::IsBitUpdated(int bit_index){
     int byte_index = 0;
     if (bit_index>=8){
         byte_index++;
         bit_index -=8;
     }
-    if ((CurrentFlags[byte_index] ^ LastFlags[byte_index]) & (1<<bit_index))
+    int bit_mask = (CurrentFlags[byte_index] ^ LastFlags[byte_index]) & (1<<bit_index);
+    if (bit_mask > 0){
+        // Logger::Debug("TouchCell::IsBitUpdated()");
+        // Logger::Print("Cell address", Address);
+        // Logger::Print("Current Flag 0", CurrentFlags[0]);
+        // Logger::Print("Last Flag 0", LastFlags[0]);
+        // Logger::Print("Current Flag 1", CurrentFlags[1]);
+        // Logger::Print("Last Flag 1", LastFlags[1]);
+        // Logger::Print("bit_mask", bit_mask);
+        // Logger::Print("Updated Byte index", byte_index);
+        // Logger::Print("Updated Bit_index", bit_index);
         return true;
+    }
     return false;
 }
 const char* TouchCell::GetMqttPayload(int bit_index){
@@ -72,11 +84,4 @@ const char* TouchCell::GetMqttPayload(int bit_index){
     return "OFF";
 }
 
-void TouchCell::PrintOut(const char* title){
-    return;
-    Serial.print("\n======================= TouchCell::PrintOut()===================\n");
-    Serial.println(title);
-    Serial.print("\n    Addr = ");
-    Serial.print(this->Address);
 
-}
--- esp32_arm/src/MyApps/acupuncture/touch_cell.h
+++ esp32_arm/src/MyApps/acupuncture/touch_cell.h
@@ -8,19 +8,16 @@ class TouchCell{
         uint8_t CurrentFlags[4];
         uint8_t LastFlags[4];
         bool HasUpdate();
-        bool CompareCurrentAndLast();
         void CopyCurrentToLast();
         const char* GetName(int point_id);
         bool IsBitUpdated(int bit_index);
         const char* GetMqttPayload(int bit_index);
-        void PrintOut(const char* title);
-        bool auto_offline = true;
+        bool IsForceOnline = false;
         const bool& IsOnline = _is_online;
         void SetOffline(){this->_is_online = false;};
         void SetOnline(){this->_is_online= true;};
         
     private:
-        bool _has_update;
         bool _is_online = true;
         char __mqtt_topic_substring[30];
 
