b7a9fb810b41a3c0cf612305b7ffeb56277d0c0e
Xuming Feng <voicevon@gmail.com>
2023-03-31 12:09:35 +0800

    deploying test

--- apps/twh2/main_wms.py
+++ apps/twh2/main_wms.py
@@ -3,19 +3,19 @@ from wcs_robots.twh_wcs import Start_WCS_Process
 from web_user.route import web_user
 from web_stock.route import web_stock
 
-web = Flask(__name__)
-web.config['SECRET_KEY'] = '20221220'
-web.secret_key = '20221221'   # for WTForm
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '20221220'
+app.secret_key = '20221221'   # for WTForm
 
-web.register_blueprint(web_user)
-web.register_blueprint(web_stock)
+# app.register_blueprint(web_user)
+# app.register_blueprint(web_stock)
 
 # web.debug=False
 new_message = {}
 new_message['test'] = {'id':2, 'payload': 'hello, dynamic message.'}
 
 
-@web.route('/create_map_for_new_twh')
+@app.route('/create_map_for_new_twh')
 def create_map_for_new_twh():
     store_map = []
     return render_template('create_map_for_new_twh.html', store_map = store_map)
@@ -28,4 +28,4 @@ if __name__ == '__main__':
     # https://stackoverflow.com/questions/31264826/start-a-flask-application-in-separate-thread
     #  web.run(debug=True)
 
-    web.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False)
--- apps/twh2/route.py
+++ apps/twh2/route.py
@@ -1,2 +1,46 @@
 # https://github.com/abalarin/Flask-on-Linode
 
+from flask import Flask,  render_template
+from wcs_robots.twh_wcs import Start_WCS_Process
+from web_user.route import web_user
+from web_stock.route import web_stock
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '20221220'
+app.secret_key = '20221221'   # for WTForm
+
+app.register_blueprint(web_user)
+app.register_blueprint(web_stock)
+
+# app.debug=False
+new_message = {}
+new_message['test'] = {'id':2, 'payload': 'hello, dynamic message.'}
+
+
+@app.route('/')
+def index():
+    store_map = []
+    return render_template('index.html', store_map = store_map)
+
+@app.route('/contact')
+def contact():
+    return render_template('cotact.html')        
+
+@app.route('/product_wcs')
+def product_wcs():
+    return render_template('product_wcs.html')
+
+@app.route('/product_acupuncture.html')
+def product_acupuncture():
+    return render_template('product_acupuncture.html')
+
+
+
+Start_WCS_Process()
+
+if __name__ == '__main__':
+    # reloader or debug must be false.  
+    # https://stackoverflow.com/questions/31264826/start-a-flask-application-in-separate-thread
+    #  web.run(debug=True)
+
+    app.run(host='0.0.0.0', debug=False)
