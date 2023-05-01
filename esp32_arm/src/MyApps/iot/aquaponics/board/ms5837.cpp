1eed52127695ba32db3e262552084da67c039280
Xuming <voicevon@gmail.com>
2023-05-01 11:23:24 +0800

    minus

--- esp32_arm/src/MyApps/iot/aquaponics/board/ms5837.cpp
+++ esp32_arm/src/MyApps/iot/aquaponics/board/ms5837.cpp
@@ -8,15 +8,17 @@ void Ms5837::Init(){
 }
 
 void Ms5837::SpinOnce(){
-    float sensor_value = __GetDepth_mm();
+    float sensor_value = __ReadSensor();
     if (sensor_value != __previous_sensor_value){
-        g_mqttClient.publish(_mqtt_publish_topic, 2, true, sensor_value);
+        String payload="";
+        payload.concat(sensor_value);
+        g_mqttClient.publish(_mqtt_publish_topic, 2, true, payload.c_str());
         __previous_sensor_value = sensor_value;
     }
 }
 
 
-float Ms5837::__GetDepth_mm(){
+float Ms5837::__ReadSensor(){
     return 1.234;
 }
 
