0451bd3ad786db61080366af5711ee14ba4743db
Xuming <voicevon@gmail.com>
2023-05-05 12:46:47 +0800

    latency bug

--- apps/cloud_kvm/frontier/rpi_mqtt/rpi_kvm.py
+++ apps/cloud_kvm/frontier/rpi_mqtt/rpi_kvm.py
@@ -5,8 +5,9 @@ from von.remote_var_mqtt import RemoteVar_mqtt
 
 import time
 
-# os = 'Windows'
-os = 'Linux'
+os = 'Windows'
+os = 'Linux_desktop'
+os = 'Pi_lite'
 
 
 if __name__ == '__main__':
@@ -19,23 +20,29 @@ if __name__ == '__main__':
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     # https://stackoverflow.com/questions/52068277/change-frame-rate-in-opencv-3-4-2
-    cap.set(cv2.CAP_PROP_FPS, 10)
-    fps = int(cap.get(5))
-    print("fps:", fps)
+    # cap.set(cv2.CAP_PROP_FPS, 10)
+    # fps = int(cap.get(5))
+    # print("fps:", fps)
 
     g_mqtt_broker_config.client_id = "230504"
     g_mqtt.connect_to_broker(g_mqtt_broker_config)
 
-    fps_limit = RemoteVar_mqtt("ocr/ubuntu_performance/kvm_config", 2)
+    fps_limit = RemoteVar_mqtt("ocr/ubuntu_performance/kvm_config", 1)
+    Logger.Print("fps_limit", fps_limit.get())
     publish_counter = 0
     start_time = 0
     while True:
         ret, frame = cap.read()
         if ret:
             now_time = time.time()
-            if int(now_time - start_time) > fps_limit.get():
+            if int(now_time - start_time) > int(fps_limit.get()):
+                if os !='Pi_lite':
+                    cv2.imshow('camera', frame)
+                    cv2.waitKey(1)
                 g_mqtt.publish_cv_image("ocr/ubuntu_performance/screen_image",  frame)
-                Logger.Print("publish counter", publish_counter)
                 start_time = time.time()
+
+                publish_counter += 1
+                Logger.Print("publish counter", publish_counter)
 
 
