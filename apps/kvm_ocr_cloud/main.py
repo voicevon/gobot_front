ff00794b0664c7fd13684d767b29794af23c4450
Xuming <voicevon@gmail.com>
2023-05-10 11:02:25 +0800

    a milestone

--- apps/kvm_ocr_cloud/main.py
+++ apps/kvm_ocr_cloud/main.py
@@ -2,18 +2,20 @@ from von.mqtt.remote_var_mqtt import g_mqtt,g_mqtt_broker_config
 from ocr_node_factory import OcrNodeFactory
 import time
 
+# who_amd_i = "auto runner on rasp-pi"
+who_am_i = "normal"
+
 if __name__ == "__main__":
-    
-    time.sleep(10)
-    g_mqtt_broker_config.client_id = "230510aaddf"
-    g_mqtt.connect_to_broker(g_mqtt_broker_config, blocked_connection=True)
+        if who_am_i == "auto runner on rasp-pi":
+            time.sleep(10)
 
-    # ocr_node = CreateOcrNode('marker')
-    # ocr_node = OcrNodeFactory.CreateOcrNode('def_areas', 'kvm_230508', 'windows_performance',is_new_kvm_node=False, is_new_app_window=False )
-    # ocr_node = OcrNodeFactory.CreateOcrNode('all_in_one', 'kvm_230508','windows_performance', is_new_kvm_node= False, is_new_app_window=False)
-    ocr_node = OcrNodeFactory.CreateOcrNode('demo_yjg', 'demo_yjg','temp_window', is_new_kvm_node= False, is_new_app_window=False)
-    # ocr_node = OcrNodeFactory.CreateOcrNode('demo_fxm', 'demo_yjg','temp_window', is_new_kvm_node=True, is_new_app_window=False)
-    # ocr_node = OcrNodeFactory.CreateOcrNode('view_demo', 'demo_yjg','temp_window', is_new_kvm_node=False, is_new_app_window=False)
+        g_mqtt.connect_to_broker(g_mqtt_broker_config, blocked_connection=True)
+        # ocr_node = CreateOcrNode('marker')
+        # ocr_node = OcrNodeFactory.CreateOcrNode('def_areas', 'kvm_230508', 'windows_performance',is_new_kvm_node=False, is_new_app_window=False )
+        # ocr_node = OcrNodeFactory.CreateOcrNode('all_in_one', 'kvm_230508','windows_performance', is_new_kvm_node= False, is_new_app_window=False)
+        # ocr_node = OcrNodeFactory.CreateOcrNode('demo_yjg', 'demo_yjg','temp_window', is_new_kvm_node= False, is_new_app_window=False)
+        # ocr_node = OcrNodeFactory.CreateOcrNode('demo_fxm', 'demo_yjg','temp_window', is_new_kvm_node=True, is_new_app_window=False)
+        ocr_node = OcrNodeFactory.CreateOcrNode('view_demo', 'demo_yjg','temp_window', is_new_kvm_node=False, is_new_app_window=False)
 
-    while True:
-        ocr_node.SpinOnce()
+        while True:
+            ocr_node.SpinOnce()
