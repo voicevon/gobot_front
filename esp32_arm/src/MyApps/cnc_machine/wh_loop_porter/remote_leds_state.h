4d69ed987cc8fa8db806234d5f3dcd96c74a7df3
Xuming <voicevon@gmail.com>
2023-05-22 22:43:15 +0800

    json to array

--- esp32_arm/src/MyApps/cnc_machine/wh_loop_porter/remote_leds_state.h
+++ esp32_arm/src/MyApps/cnc_machine/wh_loop_porter/remote_leds_state.h
@@ -1,14 +1,16 @@
 #include "Mqtt/mqtt_subscriber_manager.h"
 
+#define __JSON_BUFFER_SIZE 60
 class RemoteLedsState: public MqttSubscriberBase{
     public:
         bool GotRemoteValue(){return __got_remote_value;};
-        float Get();
+        char* Get();
 
     private:
         void onGot_MqttMessage(const char* payload, uint16_t payload_len) override;
         // void __onMessage(const char* payload, uint16_t payload_len);
-        float __remote_value = 0.0f;
+        char __mqtt_payload[__JSON_BUFFER_SIZE];
         bool __got_remote_value = false;
+        char __remote_value[7];
 
 };
