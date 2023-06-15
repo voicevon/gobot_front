21ad4ff77630a8f0bfa3bae546bae93d958cb194
wx225117 <1398375529@qq.com>
2023-06-15 21:20:12 +0800

    lua minus

--- esp32_arm/data/water_dropper.lua
+++ esp32_arm/data/water_dropper.lua
@@ -1,10 +1,18 @@
-p = 0
+p = 0 
 i = 0 
 state = 1 
 timer = 0 
-origin_angle = 0.1 
 starting = 0 
-speed = 10.1
+speed = 10
+zero_point = 0.1
+MAX_SPEED = 255
+MIN_SPEED = 0
+logic_speed = 1.1
+
+k_angle = 1.1
+k_speed = 5.1
+
+require "math"
 
 function reached_timer()
     now = hw.millis()
@@ -23,18 +31,28 @@ function test_all()
     a = wd.readEncoderAngle()
     b = wd.readButton()
     t = wd.readDelaySecond()
-    print(i, a, b, t, state, hw.millis()-starting, speed)
+    print("i=",i, "angle=",a, "button=", b, t, "state=",state, "zero_point=", zero_point, hw.millis()-starting, speed, logic_speed)
+end
+
+
+function get_angle()
+    return  5.1 * (wd.readEncoderAngle() - zero_point)
+end
+
+function set_speed()
+     wd.setMotorSpeed(Math.floor(MAX_SPEED - logic_speed))
 end
 
 function at_state_idle()
-    angle = wd.readEncoderAngle()
+    angle = get_angle()
     if angle > 10 then 
-        origin_angle = angle 
         state = 3 
-    return end
+        return 
+    end
     if wd.readButton() then
-        wd.setMotorSpeed(256 - 256) 
-        timer = wd.readDelaySecond() * 1000 
+        -- logic_speed = 11.2 * get_angle()
+        logic_speed = MAX_SPEED
+        timer = wd.readDelaySecond() * 1000
         starting = hw.millis() 
         state = 2 
         return
@@ -44,25 +62,29 @@ end
 function at_state_run()
     now = hw.millis()
     if now - starting > timer then 
-        wd.setMotorSpeed(256 - 0) 
+        logic_speed = MIN_SPEED 
         state = 1 
     end
 end
 
+
 function at_state_pwm()
-    angle = wd.readEncoderAngle() speed = 11.2 * (angle - origin_angle)
+    speed = 5.2 * get_angle()
     if speed > 255 then 
         speed = 255 
     end
-    if speed < 10 then 
-        wd.setMotorSpeed(256 - 0) 
+    logic_speed = speed;
+    if angle < 10 then 
+        logic_speed = MIN_SPEED
         state = 1 
     end
 end
 
 function setup()
     print("setup() is here.") 
-    wd.setMotorSpeed(256-0) 
+    wd.setMotorSpeed(256-0)
+    logic_speed = MIN_SPEED
+    zero_point = wd.readEncoderAngle()
 end
 
 function loop ()
@@ -70,4 +92,5 @@ function loop ()
     if state == 1 then at_state_idle() end
     if state == 2 then at_state_run() end
     if state == 3 then at_state_pwm() end
+    set_speed()
 end
